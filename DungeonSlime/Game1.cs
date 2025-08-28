54573695fae06e1173fe8e653eb6aba2b34f76a3
DoriXon55 <dorianzasada@gmail.com>
2025-08-28 20:50:36 +0200

    chapter 17

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -1,34 +1,13 @@
-﻿using System;
-
-using Microsoft.Xna.Framework;
-
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
+﻿using DungeonSlime.Scenes;
+using Microsoft.Xna.Framework.Media;
 using MonoGameLibrary;
-using MonoGameLibrary.Graphics;
-using MonoGameLibrary.Input;
-
 
 namespace DungeonSlime;
 
 public class Game1 : Core
 {
-
-    private AnimatedSprite _slime;
-
-    private AnimatedSprite _bat;
-
-    private Vector2 _slimePosition;
-
-    private const float MOVEMENT_SPEED = 5.0f;
-
-    private Vector2 _batPosition;
-
-    private Vector2 _batVelocity;
-
-    private Tilemap _tilemap;
-
-    private Rectangle _roomBounds;
+    // The background theme song.
+    private Song _themeSong;
 
     public Game1() : base("Dungeon Slime", 1280, 720, false)
     {
@@ -39,264 +18,16 @@ public class Game1 : Core
     {
         base.Initialize();
 
-        Rectangle screenBounds = GraphicsDevice.PresentationParameters.Bounds;
+        // Start playing the background music.
+        Audio.PlaySong(_themeSong);
 
-        _roomBounds = new Rectangle(
-            (int)_tilemap.TileWidth,
-            (int)_tilemap.TileHeight,
-            screenBounds.Width - (int)_tilemap.TileWidth * 2,
-            screenBounds.Height - (int)_tilemap.TileHeight * 2
-        );
-
-        int centerRow = _tilemap.Rows / 2;
-        int centerColumn = _tilemap.Columns / 2;
-        _slimePosition = new Vector2(centerColumn * _tilemap.TileWidth, centerRow * _tilemap.TileHeight);
-        _batPosition = new Vector2(_roomBounds.Left, _roomBounds.Top);
-        AssignRandomBatVelocity();
-
+        // Start the game with the title scene.
+        ChangeScene(new TitleScene());
     }
 
     protected override void LoadContent()
     {
-        TextureAtlas atlas = TextureAtlas.FromFile(Content, "images/atlas-definition.xml");
-        _slime = atlas.CreateAnimatedSprite("slime-animation");
-        _slime.Scale = new Vector2(4.0f, 4.0f);
-
-
-        _bat = atlas.CreateAnimatedSprite("bat-animation");
-        _bat.Scale = new Vector2(4.0f, 4.0f);
-
-        // Create the tilemap from the XML configuration file.
-        _tilemap = Tilemap.FromFile(Content, "images/tilemap-definition.xml");
-        _tilemap.Scale = new Vector2(4.0f, 4.0f);
-
-    }
-
-    protected override void Update(GameTime gameTime)
-    {
-        _slime.Update(gameTime);
-
-        _bat.Update(gameTime);
-
-        CheckKeyboardInput();
-
-        CheckGamePadInput();
-
-        
-
-        Circle slimeBounds = new Circle(
-            (int)(_slimePosition.X + (_slime.Width * 0.5f)),
-            (int)(_slimePosition.Y + (_slime.Height * 0.5f)),
-            (int)(_slime.Width * 0.5f)
-        );
-
-        // Use distance based checks to determine if the slime is within the
-        // bounds of the game screen, and if it is outside that screen edge,
-        // move it back inside.
-        if (slimeBounds.Left < _roomBounds.Left)
-        {
-            _slimePosition.X = _roomBounds.Left;
-        }
-        else if (slimeBounds.Right > _roomBounds.Right)
-        {
-            _slimePosition.X = _roomBounds.Right - _slime.Width;
-        }
-
-        if (slimeBounds.Top < _roomBounds.Top)
-        {
-            _slimePosition.Y = _roomBounds.Top;
-        }
-        else if (slimeBounds.Bottom > _roomBounds.Bottom)
-        {
-            _slimePosition.Y = _roomBounds.Bottom - _slime.Height;
-        }
-
-
-        Vector2 newBatPosition = _batPosition + _batVelocity;
-
-        Circle batBounds = new Circle(
-            (int)(newBatPosition.X + (_bat.Width * 0.5f)),
-            (int)(newBatPosition.Y + (_bat.Height * 0.5f)),
-            (int)(_bat.Width * 0.5f)
-        );
-
-        Vector2 normal = Vector2.Zero;
-
-        // Use distance based checks to determine if the bat is within the
-        // bounds of the game screen, and if it is outside that screen edge,
-        // reflect it about the screen edge normal.
-        if (batBounds.Left < _roomBounds.Left)
-        {
-            normal.X = Vector2.UnitX.X;
-            newBatPosition.X = _roomBounds.Left;
-        }
-        else if (batBounds.Right > _roomBounds.Right)
-        {
-            normal.X = -Vector2.UnitX.X;
-            newBatPosition.X = _roomBounds.Right - _bat.Width;
-        }
-
-        if (batBounds.Top < _roomBounds.Top)
-        {
-            normal.Y = Vector2.UnitY.Y;
-            newBatPosition.Y = _roomBounds.Top;
-        }
-        else if (batBounds.Bottom > _roomBounds.Bottom)
-        {
-            normal.Y = -Vector2.UnitY.Y;
-            newBatPosition.Y = _roomBounds.Bottom - _bat.Height;
-        }
-    
-
-        // If the normal is anything but Vector2.Zero, this means the bat had
-        // moved outside the screen edge so we should reflect it about the
-        // normal.
-        if (normal != Vector2.Zero)
-        {
-            _batVelocity = Vector2.Reflect(_batVelocity, normal);
-        }
-
-        _batPosition = newBatPosition;
-
-        if (slimeBounds.Intersects(batBounds))
-        {
-            // Divide the width  and height of the screen into equal columns and
-            // rows based on the width and height of the bat.
-            int totalColumns = GraphicsDevice.PresentationParameters.BackBufferWidth / (int)_bat.Width;
-            int totalRows = GraphicsDevice.PresentationParameters.BackBufferHeight / (int)_bat.Height;
-
-            // Choose a random row and column based on the total number of each
-            int column = Random.Shared.Next(0, totalColumns);
-            int row = Random.Shared.Next(0, totalRows);
-
-            // Change the bat position by setting the x and y values equal to
-            // the column and row multiplied by the width and height.
-            _batPosition = new Vector2(column * _bat.Width, row * _bat.Height);
-
-            // Assign a new random velocity to the bat
-            AssignRandomBatVelocity();
-        }
-
-        base.Update(gameTime);
-
-
-    }
-
-
-    private void AssignRandomBatVelocity()
-    {
-        // Generate a random angle.
-        float angle = (float)(Random.Shared.NextDouble() * Math.PI * 2);
-
-        // Convert angle to a direction vector.
-        float x = (float)Math.Cos(angle);
-        float y = (float)Math.Sin(angle);
-        Vector2 direction = new Vector2(x, y);
-
-        // Multiply the direction vector by the movement speed.
-        _batVelocity = direction * MOVEMENT_SPEED;
-    }
-
-    private void CheckKeyboardInput()
-    {
-        float speed = MOVEMENT_SPEED;
-
-        if (Input.Keyboard.IsKeyDown(Keys.Space))
-        {
-            speed *= 1.5f;
-        }
-        if (Input.Keyboard.IsKeyDown(Keys.W) || Input.Keyboard.IsKeyDown(Keys.Up))
-        {
-            _slimePosition.Y -= speed;
-        }
-        if (Input.Keyboard.IsKeyDown(Keys.S) || Input.Keyboard.IsKeyDown(Keys.Down))
-        {
-            _slimePosition.Y += speed;
-        }
-        if (Input.Keyboard.IsKeyDown(Keys.A) || Input.Keyboard.IsKeyDown(Keys.Left))
-        {
-            _slimePosition.X -= speed;
-        }
-        if (Input.Keyboard.IsKeyDown(Keys.D) || Input.Keyboard.IsKeyDown(Keys.Right))
-        {
-            _slimePosition.X += speed;
-        }
-    }
-
-    private void CheckGamePadInput()
-    {
-
-        GamePadInfo gamePadOne = Input.GamePads[(int)PlayerIndex.One];
-        float speed = MOVEMENT_SPEED;
-
-        if (gamePadOne.IsButtonDown(Buttons.A))
-        {
-            speed *= 1.5f;
-            GamePad.SetVibration(PlayerIndex.One, 1.0f, 1.0f);
-        }
-        else
-        {
-            GamePad.SetVibration(PlayerIndex.One, 0.0f, 0.0f);
-        }
-
-        // Check thumbstick first since it has priority over which gamepad input
-        // is movement.  It has priority since the thumbstick values provide a
-        // more granular analog value that can be used for movement.
-        if (gamePadOne.LeftThumbStick != Vector2.Zero)
-        {
-            _slimePosition.X += gamePadOne.LeftThumbStick.X * speed;
-            _slimePosition.Y -= gamePadOne.LeftThumbStick.Y * speed;
-        }
-        else
-        {
-            // If DPadUp is down, move the slime up on the screen.
-            if (gamePadOne.IsButtonDown(Buttons.DPadUp))
-            {
-                _slimePosition.Y -= speed;
-            }
-
-            // If DPadDown is down, move the slime down on the screen.
-            if (gamePadOne.IsButtonDown(Buttons.DPadDown))
-            {
-                _slimePosition.Y += speed;
-            }
-
-            // If DPapLeft is down, move the slime left on the screen.
-            if (gamePadOne.IsButtonDown(Buttons.DPadLeft))
-            {
-                _slimePosition.X -= speed;
-            }
-
-            // If DPadRight is down, move the slime right on the screen.
-            if (gamePadOne.IsButtonDown(Buttons.DPadRight))
-            {
-                _slimePosition.X += speed;
-            }
-        }
-    }
-
-
-
-    protected override void Draw(GameTime gameTime)
-    {
-        // Clear the back buffer.
-        GraphicsDevice.Clear(Color.CornflowerBlue);
-
-        // Begin the sprite batch to prepare for rendering.
-        SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
-
-        // Draw the tilemap
-        _tilemap.Draw(SpriteBatch);
-
-        // Draw the slime sprite.
-        _slime.Draw(SpriteBatch, _slimePosition);
-
-        // Draw the bat sprite.
-        _bat.Draw(SpriteBatch, _batPosition);
-
-        // Always end the sprite batch when finished.
-        SpriteBatch.End();
-
-        base.Draw(gameTime);
+        // Load the background theme music.
+        _themeSong = Content.Load<Song>("audio/theme");
     }
 }
